46c4f5d816a54125a21f415dd9493c38d75a815d
Leo Burdianov <leo.burdianov@gmail.com>
2016-10-15 00:12:43 +0400

    Add Retrofit 2 dependencies

--- app/src/main/java/com/testography/androidmiddlegot/ui/activities/SwornMemberActivity.java
+++ app/src/main/java/com/testography/androidmiddlegot/ui/activities/SwornMemberActivity.java
@@ -1,15 +1,41 @@
 package com.testography.androidmiddlegot.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.testography.androidmiddlegot.R;
 
 public class SwornMemberActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sworn_member);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setupToolbar();
+
+        mTextView = (TextView) findViewById(R.id.text_view_profile);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("name");
+        mTextView.setText(message);
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
